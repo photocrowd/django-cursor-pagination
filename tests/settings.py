066e167aab7fcebab9ee50a20591e457574dc35d
Marc Tamlyn <marc.tamlyn@gmail.com>
2016-06-01 16:19:56 +0100

    Old pg backend name.

--- tests/settings.py
+++ tests/settings.py
@@ -1,6 +1,6 @@
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django-cursor-pagination',
     }
 }
