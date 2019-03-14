1fd5ad769fbbf9ec42e8d362ec0c6e272be2b473
Anton Yefremov <anton.iefremov@arachnys.com>
2019-03-14 17:55:05 +0200

    Remove caveat of not supporting different directions

--- cursor_pagination.py
+++ cursor_pagination.py
@@ -1,29 +1,9 @@
 from base64 import b64decode, b64encode
 from collections.abc import Sequence
 
-from django.db.models import Field, Func, Value, TextField
+from django.db.models import Q, TextField, Value
 from django.utils.translation import gettext_lazy as _
-
-
-class TupleField(Field):
-    pass
-
-
-class Tuple(Func):
-    function = ''
-    output_field = TupleField()
-
-    def get_group_by_cols(self):
-        # Irrespective of whether we have an aggregate, we want to drill down
-        # to the children here. You can't GROUP BY a tuple like you would for a
-        # "normal" function - i.e. GROUP BY ("a", "b") is invalid SQL. However,
-        # it's logically equivalent to GROUP BY "a", "b" in call cases, so we
-        # never need the case where this 'function' needs to be included in the
-        # clause.
-        cols = []
-        for expr in self.source_expressions:
-            cols += expr.get_group_by_cols()
-        return cols
+from django.utils.translation import ugettext_lazy as _
 
 
 class InvalidCursor(Exception):
@@ -66,9 +46,6 @@ class CursorPaginator(object):
         self.queryset = queryset.order_by(*ordering)
         self.ordering = ordering
 
-        if not all(o.startswith('-') for o in ordering) and not all(not o.startswith('-') for o in ordering):
-            raise InvalidCursor('Direction of orderings must match')
-
     def page(self, first=None, last=None, after=None, before=None):
         qs = self.queryset
         page_size = first or last
@@ -103,12 +80,39 @@ class CursorPaginator(object):
     def apply_cursor(self, cursor, queryset, reverse=False):
         position = self.decode_cursor(cursor)
 
-        is_reversed = self.ordering[0].startswith('-')
-        queryset = queryset.annotate(_cursor=Tuple(*[o.lstrip('-') for o in self.ordering]))
-        current_position = [Value(p, output_field=TextField()) for p in position]
-        if reverse != is_reversed:
-            return queryset.filter(_cursor__lt=Tuple(*current_position))
-        return queryset.filter(_cursor__gt=Tuple(*current_position))
+        filtering = Q()
+        q_acc = []
+
+        # this was previously implemented as tuple comparison done on postgres side
+        # Assume comparing 3-tuples a and b,
+        # the comparison a < b is equivalent to:
+        # (a.0 < b.0) || (a.0 == b.0 && (a.1 < b.1)) || (a.0 == b.0 && a.1 == b.1 && (a.2 < b.2))
+        # The expression above does not depend on short-circuit evalution support,
+        # which is usually unavailable on backend RDB
+
+        for position, ordering in zip(position, self.ordering):
+
+            is_reversed = ordering.startswith('-')
+            o = ordering.lstrip('-')
+
+            if reverse != is_reversed:
+                key_cmp = "{}__lt".format(o)
+            else:
+                key_cmp = "{}__gt".format(o)
+
+            key_eq = "{}__exact".format(o)
+
+            value = Value(position, output_field=TextField())
+
+            q = {key_cmp: value}
+            for q_eq in q_acc:
+                q.update(q_eq)
+
+            filtering |= Q(**q)
+
+            q_acc.append({key_eq: value})
+
+        return queryset.filter(filtering)
 
     def decode_cursor(self, cursor):
         try:
