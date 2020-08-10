e43dee9538b2e5e4223e1dbc9564394befb4b8b3
Patrick Arminio <patrick.arminio@gmail.com>
2020-08-10 11:57:22 +0100

    Add postgres service

--- tests/settings.py
+++ tests/settings.py
@@ -2,6 +2,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django-cursor-pagination',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
