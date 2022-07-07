6e1e2421fd5c4ce5dd2cb8374d9eb7913bc9d0b6
Patrick Arminio <patrick.arminio@gmail.com>
2022-07-07 19:23:37 +0100

    Prepare for new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 setup(
     name='django-cursor-pagination',
     py_modules=['cursor_pagination'],
-    version='0.1.5',
+    version='0.2.0',
     description='Cursor based pagination for Django',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -23,10 +23,9 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
